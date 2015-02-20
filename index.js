c22a58f42e8f7c4332c3b15f37c98dbfa7b17c86
Ben Briggs <therealbenbriggs@hotmail.com>
2015-02-20 11:45:26 +0000

    Code style.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 'use strict';
 
-var size = require('filesize'),
+var size  = require('filesize'),
     gutil = require('gulp-util'),
-    path = require('path'),
-    map = require('map-stream');
+    path  = require('path'),
+    map   = require('map-stream');
 
-function bytediff() {
-    return map( function (file, cb) {
+function bytediff () {
+    return map(function (file, cb) {
         // Persist the original size of the file for later
         file.bytediff = {
             startSize: file.contents.length
@@ -21,26 +21,28 @@ function bytediff() {
 bytediff.start = bytediff;
 
 bytediff.stop = function (formatFn) {
-    return map(function(file, cb) {
+    return map(function (file, cb) {
         if (typeof formatFn !== 'function') {
             formatFn = function (data) {
                 var saved = (data.savings > 0) ? ' saved ' : ' gained ';
                 var color = (data.savings > 0) ? 'green' : 'yellow';
-                var report = ' (' + size(data.startSize) + ' -> ' + gutil.colors[color](size(data.endSize)) + ')';
+                var start = size(data.startSize);
+                var end = gutil.colors[color](size(data.endSize));
+                var report = ' (' + start + ' -> ' + end + ')';
                 return data.fileName + saved +
                   size(Math.abs(data.savings)) + report;
             };
         }
 
         var endSize = file.contents.length;
-        var data = {
+
+        gutil.log(formatFn({
             fileName: path.basename(file.path),
             startSize: file.bytediff.startSize,
             endSize: endSize,
             savings: file.bytediff.startSize - endSize,
             percent: (endSize / file.bytediff.startSize)
-        };
-        gutil.log( formatFn(data) );
+        }));
 
         cb(null, file);
     });
